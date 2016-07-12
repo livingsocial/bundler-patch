d61c752b74654786f258058944e1212089ecbc30
chrismo <chrismo@clabs.org>
2016-07-12 13:14:42 -0500

    1.13 changes to get all tests passing.
    1.13 GemVersionPromoter (at this point unreleased) now is refactored to
    have a post_sort method for overriding that behavior reliably, so
    maneuvers to get specific versions to the end in certain cases can also
    happen reliably.

--- lib/bundler/patch/gem_version_patch_promoter.rb
+++ lib/bundler/patch/gem_version_patch_promoter.rb
@@ -8,58 +8,68 @@ module Bundler::Patch
       result = super(spec_groups, locked_spec)
       return result unless locked_spec
 
-      gem_name = locked_spec.name
-      locked_version = locked_spec.version
-      gem_patch = @gems_to_update.gem_patch_for(gem_name)
-      new_version = gem_patch ? gem_patch.new_version : nil
+      @gem_name = locked_spec.name
+      @locked_version = locked_spec.version
+      gem_patch = @gems_to_update.gem_patch_for(@gem_name)
+      @new_version = gem_patch ? gem_patch.new_version : nil
 
-      return result unless @minimal || new_version
+      return result unless @minimal || @new_version
 
-      # TODO: says 'during' but will be output immediately, unlike the before/after in super class. :/
       # STDERR.puts "during sort_versions: #{debug_format_result(spec_groups.first.first.name, result).inspect}" if ENV["DEBUG_RESOLVER"]
 
       # Custom sort_by-ish behavior to minimize index calls.
-      result.map { |a| [result.index(a), a] }.sort do |(a_index, a), (b_index, b)|
-        a_ver = a.version
-        b_ver = b.version
+      result = result.map { |a| [result.index(a), a] }.sort do |(a_index, a), (b_index, b)|
+        @a_ver = a.version
+        @b_ver = b.version
         case
-        when @minimal && @gems_to_update.unlocking_gem?(gem_name) &&
-          (neither_version_matches(locked_version, a_ver, b_ver) &&
-            (!new_version || both_versions_gt_or_equal_to_version(new_version, a_ver, b_ver)))
-          b_ver <=> a_ver
-        when @minimal && @gems_to_update.unlocking_gem?(gem_name) && one_version_matches(new_version, a_ver, b_ver)
-          sort_matching_to_end(new_version, a_ver, b_ver)
+        when @minimal && unlocking_gem? &&
+          (neither_version_matches(@locked_version) &&
+            (!@new_version || both_versions_gt_or_equal_to_version(@new_version)))
+          @b_ver <=> @a_ver
         else
           a_index <=> b_index # no change in current ordering
         end
-      end.map { |a| a.last }.tap do |res|
-        # This usage of move_version_to_end is to handle an edge case: when a new_version is up
-        # a minor version, but the level is only :patch.
-        #
-        # Inline sorting this isn't working reliably because we're working with an already sorted array
-        # in Bundler proper, and not all versions are compared in quicksort of course. One specific
-        # comparison is required though to sort this to the end and it may not always happen. This code
-        # ensures it happens.
-        if new_version && @gems_to_update.unlocking_gem?(gem_name) && !@minimal && res.last.version < new_version
-          move_version_to_end(spec_groups, new_version, res)
+      end.map { |a| a.last }
+
+      post_sort(result)
+    end
+
+    def unlocking_gem?
+      @gems_to_update.unlocking_gem?(@gem_name)
+    end
+
+    def one_version_matches(match_version)
+      [@a_ver, @b_ver].include?(match_version)
+    end
+
+    def neither_version_matches(match_version)
+      !one_version_matches(match_version)
+    end
+
+    def both_versions_gt_or_equal_to_version(version)
+      version && @a_ver >= version && @b_ver >= version
+    end
+
+    # Sorting won't work properly for some specific arrangements to the end of the list because not
+    # all versions are compared in quicksort and the result isn't deterministic.
+    def post_sort(result)
+      result = super(result)
+
+      if @new_version && unlocking_gem? && segments_match(:major, @new_version, @locked_version)
+        if @minimal || (!@minimal && result.last.version < @new_version)
+          # This handles two cases:
+          # - minimal doesn't want to go past requested new_version
+          # - new_version is up a minor rev but level is :patch
+          result = move_version_to_end(result, @new_version)
         end
       end
+
+      result
     end
 
-    def neither_version_matches(match_version, a_ver, b_ver)
-      !one_version_matches(match_version, a_ver, b_ver)
-    end
-
-    def both_versions_gt_or_equal_to_version(version, a_ver, b_ver)
-      version && a_ver >= version && b_ver >= version
-    end
-
-    def move_version_to_end(specs, version, result)
-      spec_group = specs.detect { |s| s.first.version.to_s == version.to_s }
-      if spec_group
-        result.reject! { |s| s.first.version.to_s === version.to_s }
-        result << spec_group
-      end
+    def segments_match(level, a_ver, b_ver)
+      index = [:major, :minor].index(level)
+      a_ver.segments[index] == b_ver.segments[index]
     end
   end
 end

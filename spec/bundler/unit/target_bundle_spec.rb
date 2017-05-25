e1fe5549d9f03515f56f78d5820e33bb125d9e4c
chrismo <chrismo@clabs.org>
2017-05-25 00:07:05 -0500

    Additional test for Ruby version with requirement.
    Prior to 1.12, there wasn't code to support a "ruby '~> 2.3'" style
    requirement in the Gemfile, so the spec goes pending in that case, but
    otherwise this is supported.

--- spec/bundler/unit/target_bundle_spec.rb
+++ spec/bundler/unit/target_bundle_spec.rb
@@ -34,4 +34,17 @@ describe TargetBundle do
       end
     end
   end
+
+  it 'should behave with ruby requirement' do
+    if TargetBundle.bundler_version_or_higher('1.12.0')
+      conf = RbConfig::CONFIG
+      gemfile_create("~> #{conf['MAJOR']}.#{conf['MINOR']}") do |dir|
+        tb = TargetBundle.new(dir: dir, use_target_ruby: true)
+        tb.find_target_ruby_version.to_s.should == "ruby #{RUBY_VERSION}p#{RUBY_PATCHLEVEL}"
+      end
+    else
+      pending 'test irrelevant in versions prior to 1.12.0'
+      fail
+    end
+  end
 end

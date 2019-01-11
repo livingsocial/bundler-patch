d3131d1ba5cc0ba6d81739eda223a55d92fe1df5
chrismo <chrismo@clabs.org>
2019-01-11 00:07:51 -0600

    Disable test for bundler test version when 'latest'

--- spec/bundler/unit/bundler_version_spec.rb
+++ spec/bundler/unit/bundler_version_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../spec_helper'
 
 describe 'Bundler version installed' do
   it 'should be correct on Travis CI' do
-    if ENV['TRAVIS']
+    if ENV['TRAVIS'] && ENV['BUNDLER_TEST_VERSION'] != 'latest'
       Bundler::VERSION.should == ENV['BUNDLER_TEST_VERSION']
     end
   end
